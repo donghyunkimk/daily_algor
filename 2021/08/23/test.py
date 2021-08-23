032cb749ed1734e3523871212d18bb551f8ddabb
donghyunkimk <apxww@naver.com>
2021-08-23 14:12:22 +0900

    0823

--- 2021/08/23/test.py
+++ 2021/08/23/test.py
@@ -1,30 +1,38 @@
-# 모든 경우의 테이블은 동일
-table_lst = [["SI", "JAVA", "JAVASCRIPT", "SQL", "PYTHON", "C#"],
-             ["CONTENTS", "JAVASCRIPT", "JAVA", "PYTHON", "SQL", "C++"],
-             ["HARDWARE", "C", "C++", "PYTHON", "JAVA", "JAVASCRIPT"],
-             ["PORTAL", "JAVA", "JAVASCRIPT", "PYTHON", "KOTLIN", "PHP"],
-             ["GAME", "C++", "C#", "JAVASCRIPT", "C", "JAVA"]]
+# # 모든 경우의 테이블은 동일
+# table_lst = [["SI", "JAVA", "JAVASCRIPT", "SQL", "PYTHON", "C#"],
+#              ["CONTENTS", "JAVASCRIPT", "JAVA", "PYTHON", "SQL", "C++"],
+#              ["HARDWARE", "C", "C++", "PYTHON", "JAVA", "JAVASCRIPT"],
+#              ["PORTAL", "JAVA", "JAVASCRIPT", "PYTHON", "KOTLIN", "PHP"],
+#              ["GAME", "C++", "C#", "JAVASCRIPT", "C", "JAVA"]]
+#
+#
+# def solution(table, languages, preference):
+#     answer = ''
+#     max_num = 0
+#     max_lan = []
+#
+#     for e in range(5):
+#         temp_score = 0
+#         for r in range(len(languages)):
+#             if languages[r] in table_lst[e]:
+#                 temp_score += preference[r] * (6 - table_lst[e].index(languages[r]))
+#
+#         if temp_score > max_num:
+#             max_num = temp_score
+#             max_lan = [table_lst[e][0]]
+#
+#         elif temp_score == max_num:
+#             max_lan.append(table_lst[e][0])
+#
+#     max_lan.sort()
+#     answer = max_lan[0]
+#
+#     return answer
 
 
-def solution(table, languages, preference):
-    answer = ''
-    max_num = 0
-    max_lan = []
+a = ["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"]
 
-    for e in range(5):
-        temp_score = 0
-        for r in range(len(languages)):
-            if languages[r] in table_lst[e]:
-                temp_score += preference[r] * (6 - table_lst[e].index(languages[r]))
+for q in range(len(a)):
+    a[q] = a[q].split()
 
-        if temp_score > max_num:
-            max_num = temp_score
-            max_lan = [table_lst[e][0]]
-
-        elif temp_score == max_num:
-            max_lan.append(table_lst[e][0])
-
-    max_lan.sort()
-    answer = max_lan[0]
-
-    return answer
+print(a)
